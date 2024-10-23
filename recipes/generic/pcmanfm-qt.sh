d95af7f8ecba10697704005a8a4da8f4d3853dc4
xplshn <114888778+xplshn@users.noreply.github.com>
2024-10-23 14:47:07 -0300

    Update pcmanfm-qt.sh

--- recipes/generic/pcmanfm-qt.sh
+++ recipes/generic/pcmanfm-qt.sh
@@ -1,9 +1,8 @@
-pelfCreator -m "xplshn" -n "pcmanfm-qt" -p "pcmanfm-qt" -x "usr/bin/pcmanfm-qt"  -e "pcmanfm-qt.desktop" -z
+pelfCreator -m "xplshn" -n "pcmanfm-qt" -p "pcmanfm-qt" -e "pcmanfm-qt.desktop" -z
 APPDIR="$(echo pcmanfm-qt*.AppDir)"
 # Add the .DirIcon
 if ! wget -qO "$APPDIR/.DirIcon" "https://i.ibb.co/CtNbwVk/lxqt-logo.png"; then
 	echo "Unable to add .DirIcon (png, 128x128) to the AppDir"
 	exit 1
 fi
-echo "pcmanfm-qt" > "$APPDIR/entrypoint"
 "$APPDIR"/.gen
