8fed6a2a387f7c0e756fbda9a5d230ca70445744
xplshn <114888778+xplshn@users.noreply.github.com>
2024-10-23 06:54:17 -0300

    Update pcmanfm.sh

--- recipes/generic/pcmanfm.sh
+++ recipes/generic/pcmanfm.sh
@@ -1,1 +1,1 @@
-pelfCreator -m "xplshn" -n "pcmanfm" -p "pcmanfm" -x "usr/bin/pcmanfm" -e "pcmanfm.desktop"
+DEBUG=1 pelfCreator -m "xplshn" -n "pcmanfm" -p "pcmanfm" -x "usr/bin/pcmanfm" -e "pcmanfm.desktop"
