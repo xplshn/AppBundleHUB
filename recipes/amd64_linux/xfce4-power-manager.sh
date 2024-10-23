6aa9bd10735a4d2a6594c8a4a92e87d632c94806
xplshn <xplshn@murena.io>
2024-10-23 00:01:20 -0300

    Change -e argument to pelfCreator in ./xfce4-power-manager.sh
    Signed-off-by: xplshn <xplshn@murena.io>

--- recipes/amd64_linux/xfce4-power-manager.sh
+++ recipes/amd64_linux/xfce4-power-manager.sh
@@ -1,1 +1,1 @@
-pelfCreator -m "xplshn" -n "xfce4-power-manager" -p "xfce4-power-manager" -x "usr/bin/xfce4-power-manager" -e "xfce4-power-manager.desktop"
+pelfCreator -m "xplshn" -n "xfce4-power-manager" -p "xfce4-power-manager" -x "usr/bin/xfce4-power-manager" -e "xfce4-power-manager-settings.desktop"
