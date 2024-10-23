5277c0e67513813697ca82e16ce18129bdcd0a9a
xplshn <xplshn@murena.io>
2024-10-22 23:37:55 -0300

    re-enable xfce4-terminal.sh recipe
    Signed-off-by: xplshn <xplshn@murena.io>

--- recipes/amd64_linux/xfce4-terminal.sh
+++ recipes/amd64_linux/xfce4-terminal.sh
@@ -1,1 +1,1 @@
-pelfCreator -m "xplshn" -n "xfce4-terminal" -p "xfce4-terminal" -e "xfce4-terminal.desktop" -z -x "usr/bin/xfce4-terminal"
+pelfCreator -m "xplshn" -n "xfce4-terminal" -p "xfce4-terminal" -e "xfce4-terminal.desktop" -x "usr/bin/xfce4-terminal"
