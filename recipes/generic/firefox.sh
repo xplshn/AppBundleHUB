3a4d3dc99ac77f1c272b8885f4d16a811c52bc6c
xplshn <114888778+xplshn@users.noreply.github.com>
2024-10-25 09:10:48 -0300

    Update firefox.sh

--- recipes/generic/firefox.sh
+++ recipes/generic/firefox.sh
@@ -1,1 +1,1 @@
-pelfCreator -m "xplshn" -n "firefox" -p "firefox" -x "usr/bin/firefox" -e "firefox.desktop"
+pelfCreator -m "xplshn" -n "firefox" -p "firefox" -x "usr/bin/firefox usr/lib/firefox/firefox	" -e "firefox.desktop"
