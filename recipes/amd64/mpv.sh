6fc0533b8b7f6ef3ff44b810325ade6e6b4f5351
xplshn <xplshn@murena.io>
2024-10-25 09:00:34 -0300

    Use eget2 to get releases reliably
    Signed-off-by: xplshn <xplshn@murena.io>

--- recipes/amd64/mpv.sh
+++ recipes/amd64/mpv.sh
@@ -1,3 +1,3 @@
-eget2 Samueru-sama/mpv-AppImage --asset=".AppImage" --asset="^zsync" && {
-    BS2AppBundle ./mpv-AppImage
+eget2 Samueru-sama/mpv-AppImage --asset=".AppImage" --asset="^zsync" --download-only && {
+    COMPRESSION_OPTS="-l7 -C pcmaudio/waveform::flac:level=8:exhaustive --categorize=pcmaudio --metadata-compression null -S 25 -B 16 --order nilsimsa -W 12 -w 4" BS2AppBundle ./mpv-*
 }
