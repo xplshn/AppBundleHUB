346e154d9fafbcbc3cd49e3a291cbbbc615228ce
xplshn <xplshn@murena.io>
2024-10-25 00:10:06 -0300

    Use latest steam
    Signed-off-by: xplshn <xplshn@murena.io>

--- recipes/amd64/steam.sh
+++ recipes/amd64/steam.sh
@@ -1,5 +1,2 @@
-if ! wget -qO "./steam.AppImage" "https://github.com/ivan-hc/Steam-appimage/releases/download/continuous/Steam-1.0.0.81-2-3-x86_64.AppImage"; then
-	echo "Couldn't download Steam AppImage"
-	exit 1
-fi
-COMPRESSION_OPTS="-l7 -C zstd:level=22 --metadata-compression null -S 25 -B 8 --order nilsimsa -W 12 -w 4" BS2AppBundle ./steam.AppImage
+eget2 github.com/ivan-hc/Steam-appimage --asset=".AppImage" --asset="^zsync" --download-only
+COMPRESSION_OPTS="-l7 -C pcmaudio/waveform::flac:level=8:exhaustive --categorize=pcmaudio --metadata-compression null -S 25 -B 16 --order nilsimsa -W 12 -w 4" BS2AppBundle ./Steam*.AppImage
