ffa62a2c477891ec3e61bc95645f0ec60a1f62cb
xplshn <114888778+xplshn@users.noreply.github.com>
2024-10-26 12:02:39 -0300

    Update mpv.sh

--- recipes/amd64/mpv.sh
+++ recipes/amd64/mpv.sh
@@ -1,3 +1,3 @@
-eget2 Samueru-sama/mpv-AppImage --asset=".AppImage" --asset="^zsync" --download-only && {
+eget2 github.com/Samueru-sama/mpv-AppImage --asset=".AppImage" --asset="^zsync" --download-only && {
     COMPRESSION_OPTS="-l7 -C pcmaudio/waveform::flac:level=8:exhaustive --categorize=pcmaudio --metadata-compression null -S 25 -B 16 --order nilsimsa -W 12 -w 4" BS2AppBundle ./mpv-*
 }
