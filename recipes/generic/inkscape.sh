f3fe31076a5a3ea7f199aa756501f4a246dfd1c7
xplshn <114888778+xplshn@users.noreply.github.com>
2024-10-26 03:19:53 -0300

    Update inkscape.sh

--- recipes/generic/inkscape.sh
+++ recipes/generic/inkscape.sh
@@ -1,1 +1,7 @@
-pelfCreator -m "xplshn" -n "inkscape" -p "inkscape" -e "org.inkscape.Inkscape.desktop"
+pelfCreator -m "xplshn" -n "inkscape" -p "inkscape" -e "org.inkscape.Inkscape.desktop" -z && {
+  APPDIR="$(echo inkscape*.AppDir)"
+  "$APPDIR/AppRun" --Xbwrap --uid "0" --gid "0" -- apk -X "http://dl-cdn.alpinelinux.org/alpine/edge/main" -U --allow-untrusted add adwaita-icon-theme
+  "$APPDIR/AppRun" --Xbwrap --uid "0" --gid "0" -- gtk-update-icon-cache /usr/share/icons/Adwaita
+  "$APPDIR/AppRun" --Xbwrap --uid "0" --gid "0" -- gtk-update-icon-cache /usr/share/icons/hicolor
+  "$APPDIR/.gen"
+}
