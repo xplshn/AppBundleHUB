104d9bf9437ff29c9be6ab57114111d82f90c2b1
xplshn <114888778+xplshn@users.noreply.github.com>
2024-10-26 11:02:16 -0300

    Update ppsspp.sh

--- recipes/generic/ppsspp.sh
+++ recipes/generic/ppsspp.sh
@@ -1,2 +1,1 @@
-pelfCreator -m "xplshn" -n "ppsspp" -p "ppsspp" -e "PPSSPPQt.desktop"
-pelfCreator -m "xplshn" -n "ppsspp-sharun" -p "ppsspp" -x "usr/bin/PPSSPPQt" -e "PPSSPPQt.desktop"
+#pelfCreator -m "xplshn" -n "ppsspp" -p "ppsspp" -e "PPSSPPQt.desktop"
