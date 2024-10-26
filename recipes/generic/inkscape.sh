157106c6da1fcc24d2afb0e1863dc9ca4c0a0f1e
xplshn <114888778+xplshn@users.noreply.github.com>
2024-10-26 03:13:15 -0300

    Update inkscape.sh

--- recipes/generic/inkscape.sh
+++ recipes/generic/inkscape.sh
@@ -1,2 +1,1 @@
 pelfCreator -m "xplshn" -n "inkscape" -p "inkscape" -e "org.inkscape.Inkscape.desktop"
-pelfCreator -m "xplshn" -n "inkscape-sharun" -p "inkscape" -x "usr/bin/inkscape" -e "org.inkscape.Inkscape.desktop"
