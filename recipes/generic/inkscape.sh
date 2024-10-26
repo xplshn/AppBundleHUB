127da1b7966fce8106b6cd8d13305574a625e144
xplshn <xplshn@murena.io>
2024-10-26 01:12:19 -0300

    Added inkscape
    Signed-off-by: xplshn <xplshn@murena.io>

--- recipes/generic/inkscape.sh
+++ recipes/generic/inkscape.sh
@@ -1,2 +1,2 @@
-pelfCreator -m "xplshn" -n "inkscape" -p "inkscape" -e "org.inkscape.Inkscape.desktop.desktop"
-pelfCreator -m "xplshn" -n "inkscape-sharun" -p "inkscape" -x "usr/bin/inkscape" -e "org.inkscape.Inkscape.desktop.desktop"
+pelfCreator -m "xplshn" -n "inkscape" -p "inkscape" -e "org.inkscape.Inkscape.desktop"
+pelfCreator -m "xplshn" -n "inkscape-sharun" -p "inkscape" -x "usr/bin/inkscape" -e "org.inkscape.Inkscape.desktop"
