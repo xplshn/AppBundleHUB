d534ba3d04c0740002edead3ec1de518a563fdbe
xplshn <xplshn@murena.io>
2024-10-28 02:28:05 -0300

    Added wezterm recipe
    Signed-off-by: xplshn <xplshn@murena.io>

---
+++ recipes/generic/wezterm.sh
@@ -0,0 +1,7 @@
+pelfCreator -m "xplshn" -n "wezterm" -p "wezterm" -e "wezterm-gui" -x "usr/bin/wezterm-gui" -z && {
+    APPDIR="$(echo wezterm*.AppDir)"
+    if ! wget -qO "$APPDIR/.DirIcon" "https://i.ibb.co/0MfXRdq/icon.png"; then
+    	echo "Failed to 128x128 icon"
+    	exit 1
+    fi
+}
