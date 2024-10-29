962a5e22ffbb0594e2c0c7e326e94000701d8dde
xplshn <114888778+xplshn@users.noreply.github.com>
2024-10-29 04:14:06 -0300

    Update floorp.sh

--- recipes/cachyos_generic/floorp.sh
+++ recipes/cachyos_generic/floorp.sh
@@ -1,1 +1,1 @@
-AUR=1 arch_x86_64 sh -c "ls "\$LOCAL_DIR"" #pelfCreator -m "xplshn" -n "floorp" -p "floorp-bin" -e "floorp.desktop"
+AUR=1 arch_x86_64 sh -c "ls \$LOCAL_DIR" #pelfCreator -m "xplshn" -n "floorp" -p "floorp-bin" -e "floorp.desktop"
