17091e1d24160a5e4e331eb5ccb76fb7a3335bc3
xplshn <114888778+xplshn@users.noreply.github.com>
2024-10-29 19:24:21 -0300

    Update polybar.sh

--- recipes/generic/polybar.sh
+++ recipes/generic/polybar.sh
@@ -1,1 +1,1 @@
-pelfCreator  -m "xplshn" -n "polybar" -p "polybar pipewire-pulse" -e "polybar" -x "usr/bin/polybar" -t "etc/polybar"
+pelfCreator -m "xplshn" -n "polybar" -p "polybar" -e "polybar" -x "usr/bin/polybar" -t "etc/polybar"
