ccaf220aad0d910787437e97cbb65f974cd4909a
xplshn <114888778+xplshn@users.noreply.github.com>
2024-10-29 06:06:30 -0300

    Update ungoogled-chromium.sh

--- recipes/cachyos_generic/ungoogled-chromium.sh
+++ recipes/cachyos_generic/ungoogled-chromium.sh
@@ -1,1 +1,1 @@
-arch_x86_64 pelfCreator -m "xplshn" -n "ungoogled-chromium-xdg" -p "ungoogled-chromium-xdg-bin" -e "chromium.desktop"
+AUR=1 arch_x86_64 pelfCreator -m "xplshn" -n "ungoogled-chromium" -p "ungoogled-chromium-bin" -e "chromium.desktop"
