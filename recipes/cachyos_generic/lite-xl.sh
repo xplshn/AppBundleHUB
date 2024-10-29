3309ef90130f3a9794e6edc48d2e2d2924e7dab5
xplshn <114888778+xplshn@users.noreply.github.com>
2024-10-29 06:06:03 -0300

    Update lite-xl.sh

--- recipes/cachyos_generic/lite-xl.sh
+++ recipes/cachyos_generic/lite-xl.sh
@@ -1,1 +1,1 @@
-arch_x86_64 pelfCreator -m "xplshn" -n "lite-xl" -p "lite-xl" -e "org.lite_xl.lite_xl.desktop"
+AUR=1 arch_x86_64 pelfCreator -m "xplshn" -n "lite-xl" -p "lite-xl" -e "org.lite_xl.lite_xl.desktop"
