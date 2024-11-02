1b5025017e38f1927f162b6c37b31c6507ad41cb
xplshn <114888778+xplshn@users.noreply.github.com>
2024-11-02 19:14:54 -0300

    Update scrcpy.sh

--- recipes/generic/scrcpy.sh
+++ recipes/generic/scrcpy.sh
@@ -1,1 +1,1 @@
-pelfCreator -m "xplshn" -n "scrcpy" -p "scrcpy android-tools" -x "usr/bin/scrcpy usr/bin/adb usr/bin/fastboot" -k "usr/share/scrcpy usr/share/android-tools" -e "scrcpy.desktop"
+pelfCreator -m "xplshn" -n "scrcpy" -p "scrcpy android-tools" -x "usr/bin/scrcpy usr/bin/adb usr/bin/fastboot" -k "usr/share/scrcpy usr/share/android-tools" -e "env -i -c scrcpy"
