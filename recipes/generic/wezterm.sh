0949adaac63b44b1c764344f227149a54a090ddf
xplshn <114888778+xplshn@users.noreply.github.com>
2024-11-08 17:52:21 -0300

    Update wezterm.sh

--- recipes/generic/wezterm.sh
+++ recipes/generic/wezterm.sh
@@ -1,5 +1,5 @@
 pelfCreator -m "xplshn" -n "org.wezfurlong.wezterm" -p "wezterm" -e "wezterm-gui" -x "usr/bin/wezterm-gui" -z && {
-    APPDIR="$(echo wezterm*.AppDir)"
+    APPDIR="$(echo org.wezfurlong.wezterm*.AppDir)"
     if ! wget -qO "$APPDIR/.DirIcon" "https://i.ibb.co/0MfXRdq/icon.png"; then
     	echo "Failed to 128x128 icon"
     	exit 1
