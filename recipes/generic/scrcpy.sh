4bce84c4d58c00d2bfa15b88149ab131806ed695
xplshn <114888778+xplshn@users.noreply.github.com>
2024-11-08 17:53:56 -0300

    Update scrcpy.sh

--- recipes/generic/scrcpy.sh
+++ recipes/generic/scrcpy.sh
@@ -1,4 +1,4 @@
-pelfCreator -m "xplshn" -n "scrcpy" -p "scrcpy android-tools" -x "usr/bin/scrcpy usr/bin/adb usr/bin/fastboot" -k "usr/share/scrcpy usr/share/android-tools" -e "scrcpy.desktop" -z && {
+pelfCreator -m "xplshn" -n "scrcpy" -p "scrcpy pipewire-pulse android-tools" -x "usr/bin/scrcpy usr/bin/adb usr/bin/fastboot" -k "usr/share/scrcpy usr/share/android-tools" -e "scrcpy.desktop" -z && {
     APPDIR="$(echo scrcpy*.AppDir)"
     echo "scrcpy" >"$APPDIR/entrypoint"
     "$APPDIR/.gen"
