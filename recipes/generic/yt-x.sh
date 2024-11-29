460fe7878172dd275d1f5b708f1126b0f27729f6
xplshn <114888778+xplshn@users.noreply.github.com>
2024-11-29 15:11:25 -0300

    Create yt-x.sh

---
+++ recipes/generic/yt-x.sh
@@ -0,0 +1,5 @@
+pelfCreator  -m "xplshn" -n "yt-x" -p "bash" -e "yt-x" -z && {
+  APPDIR="yt-x*.AppDir"
+  DBIN_INSTALL_DIR="./$APPDIR/proto/usr/local/bin" sh -c "dbin list | grep 'jq\|curl\|yt-dlp\|fzf\|gum\|chafa\|icat\|imgcat' | xargs dbin add"
+  "$APPDIR/.gen"
+} # FFMPEG could be pre-installed, but I don't want to fuck around and find out, you know... Shitty FFMPEG license (GPL) + binary blobs...
