d80d3fe6158fcc766bd7f8b7d61d79ae63be6eaf
xplshn <114888778+xplshn@users.noreply.github.com>
2024-11-30 11:56:08 -0300

    Update yt-x.sh

--- recipes/generic/yt-x.sh
+++ recipes/generic/yt-x.sh
@@ -1,5 +1,6 @@
-pelfCreator  -m "xplshn" -n "yt-x" -p "bash" -e "yt-x" -z && {
-  APPDIR="$(echo yt-x*.AppDir)"
-  DBIN_INSTALL_DIR="./$APPDIR/proto/usr/local/bin" sh -c "dbin list | grep 'jq\|curl\|yt-dlp\|fzf\|gum\|chafa\|icat\|imgcat' | xargs dbin add"
-  "$APPDIR/.gen"
-} # FFMPEG could be pre-installed, but I don't want to fuck around and find out, you know... Shitty FFMPEG license (GPL) + binary blobs...
+COMPRESSION_OPTS="-l7 -C pcmaudio/waveform::flac:level=8:exhaustive --categorize=pcmaudio --metadata-compression null -S 25 -B 16 --order nilsimsa -W 12 -w 4" \
+  pelfCreator  -m "xplshn" -n "yt-x" -p "bash" -e "yt-x" -z && {
+    APPDIR="$(echo yt-x*.AppDir)"
+    DBIN_INSTALL_DIR="./$APPDIR/proto/usr/local/bin" sh -c "dbin list | grep 'jq\|curl\|yt-dlp\|ffmpeg\|fzf\|gum\|chafa\|icat\|imgcat' | xargs dbin add"
+    "$APPDIR/.gen"
+  }
