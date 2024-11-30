e5d9434b88208a03b4d5edc96cbc78cfc900ac46
xplshn <114888778+xplshn@users.noreply.github.com>
2024-11-30 10:53:50 -0300

    Update inkscape.sh

--- recipes/generic/inkscape.sh
+++ recipes/generic/inkscape.sh
@@ -1,6 +1,7 @@
-pelfCreator -m "xplshn" -n "org.inkscape.Inkscape" -p "inkscape adwaita-icon-theme" -e "org.inkscape.Inkscape.desktop" -z && {
+pelfCreator -m "xplshn" -n "org.inkscape.Inkscape" -p "inkscape adwaita-icon-theme" -e "org.inkscape.Inkscape.desktop" -s -z && {
   APPDIR="$(echo org.inkscape.Inkscape*.AppDir)"
   "$APPDIR/AppRun" --Xbwrap --uid "0" --gid "0" -- gtk-update-icon-cache /usr/share/icons/Adwaita
   "$APPDIR/AppRun" --Xbwrap --uid "0" --gid "0" -- gtk-update-icon-cache /usr/share/icons/hicolor
+  wget -qO "$APPDIR/AppRun" "https://raw.githubusercontent.com/xplshn/pelf/refs/heads/pelf-ng/assets/AppRun.sharun.ovfsProto"
   "$APPDIR/.gen"
 }
