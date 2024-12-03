e8c916a15db7257be6af3dc8a7e05140544a57ec
xplshn <114888778+xplshn@users.noreply.github.com>
2024-12-03 15:14:15 -0300

    Update onlyoffice.sh

--- recipes/cachyos_generic/onlyoffice.sh
+++ recipes/cachyos_generic/onlyoffice.sh
@@ -1,1 +1,1 @@
-AUR=1 arch_x86_64 pelfCreator -m "xplshn" -n "org.onlyoffice.desktopeditors" -p "onlyoffice-bin" -e "onlyoffice-desktopeditors.desktop"
+AUR=1 arch_x86_64 pelfCreator -m "xplshn" -n "org.onlyoffice.desktopeditors" -p "onlyoffice-bin" -e "onlyoffice-desktopeditors.desktop" -s
