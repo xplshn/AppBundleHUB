bc4036839aacb310fef961d9b5de274b88b427b7
xplshn <114888778+xplshn@users.noreply.github.com>
2024-12-08 21:59:16 -0300

    Update prismlauncher.sh

--- recipes/cachyos_generic/prismlauncher.sh
+++ recipes/cachyos_generic/prismlauncher.sh
@@ -1,1 +1,1 @@
-arch_x86_64 pelfCreator -m "xplshn" -n "org.prismlauncher.PrismLauncher" -p "prismlauncher" -e "org.prismlauncher.PrismLauncher.desktop" -s
+arch_x86_64 pelfCreator -m "xplshn" -n "org.prismlauncher.PrismLauncher" -p "prismlauncher jre21-openjdk jre-openjdk" -e "org.prismlauncher.PrismLauncher.desktop" -s
