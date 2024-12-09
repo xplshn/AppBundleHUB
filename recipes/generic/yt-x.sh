09a4e6570e1f804fa6dea0c5a7e9a8f0023780e3
xplshn <114888778+xplshn@users.noreply.github.com>
2024-12-08 23:27:03 -0300

    Update yt-x.sh

--- recipes/generic/yt-x.sh
+++ recipes/generic/yt-x.sh
@@ -2,5 +2,6 @@ COMPRESSION_OPTS="-l7 -C pcmaudio/waveform::flac:level=8:exhaustive --categorize
   pelfCreator  -m "xplshn" -n "yt-x" -p "bash" -e "yt-x" -z && {
     APPDIR="$(echo yt-x*.AppDir)"
     DBIN_INSTALL_DIR="./$APPDIR/proto/usr/local/bin" sh -c "dbin list | grep 'jq\|curl\|yt-dlp\|ffmpeg\|fzf\|gum\|chafa\|icat\|imgcat' | xargs dbin add"
+    wget -qO "./$APPDIR/proto/usr/local/bin/yt-x" "https://github.com/Benexl/yt-x/raw/refs/heads/master/yt-x"
     "$APPDIR/.gen"
   }
