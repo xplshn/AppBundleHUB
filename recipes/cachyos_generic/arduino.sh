a8a77545a22633639695e17ea005c097062a9b2c
xplshn <114888778+xplshn@users.noreply.github.com>
2025-01-16 19:44:54 +0000

    Update arduino.sh

--- recipes/cachyos_generic/arduino.sh
+++ recipes/cachyos_generic/arduino.sh
@@ -1,1 +1,1 @@
-AUR=0 arch_x86_64 pelfCreator -m "xplshn" -n "cc.arduino.IDE2" -p "arduino-ide" -e "arduino-ide.desktop" -s
+AUR=0 arch_x86_64 pelfCreator -m "xplshn" -n "cc.arduino.IDE2" -p "arduino-ide" -e "arduino-ide.desktop" -x "usr/bin/arduino-ide"
