fb8de913bb9530c00e4ee4ba6bc2a105804f50c8
xplshn <xplshn@murena.io>
2025-04-01 04:34:38 -0300

    www: update ./build.sh
    Signed-off-by: xplshn <xplshn@murena.io>

--- www/build.sh
+++ www/build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+curl -LlO https://github.com/xplshn/dbin-metadata/raw/refs/heads/master/misc/cmd/modMetadata/METADATA_amd64_linux.json
+curl -LlO https://github.com/xplshn/dbin-metadata/raw/refs/heads/master/misc/cmd/modMetadata/METADATA_arm64_linux.json
 rm -rf ./dist && esbuild --bundle --outdir=dist --minify --sourcemap *.js ./*/*.js
