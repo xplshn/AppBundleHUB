ec125be8f5ad8c86e99af00d19fa4459cc3ac404
xplshn <114888778+xplshn@users.noreply.github.com>
2025-04-05 18:13:43 +0000

    Update yt-x.sh

--- recipes/generic/yt-x.sh
+++ recipes/generic/yt-x.sh
@@ -1,7 +1,7 @@
-COMPRESSION_OPTS="-l7 -C pcmaudio/waveform::flac:level=8:exhaustive --categorize=pcmaudio --metadata-compression null -S 25 -B 16 --order nilsimsa -W 12 -w 4" \
-  pelfCreator  -m "xplshn" -n "yt-x" -p "bash" -e "yt-x" -z && {
+pelfCreator  -m "xplshn" -n "yt-x" -p "bash" -e "yt-x" -z && {
     APPDIR="$(echo yt-x*.AppDir)"
+    ID="$(basename "$APPDIR")"
     DBIN_INSTALL_DIR="./$APPDIR/proto/usr/local/bin" sh -c "dbin list | grep 'jq\|curl\|yt-dlp\|ffmpeg\|fzf\|gum\|chafa\|icat\|imgcat' | xargs dbin add"
     wget -qO "./$APPDIR/proto/usr/local/bin/yt-x" "https://github.com/Benexl/yt-x/raw/refs/heads/master/yt-x"
-    "$APPDIR/.gen"
-  }
+    pelf --add-appdir "$APPDIR" --appbundle-id "$ID" --output-to "$ID.dwfs.AppBundle"
+}
