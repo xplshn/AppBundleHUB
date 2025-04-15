04d3cbbb2466c61813860c15370abc96a308e475
xplshn <114888778+xplshn@users.noreply.github.com>
2025-04-15 18:13:15 +0000

    Delete recipes/x86_64/steam.sh

--- recipes/x86_64/steam.sh
+++
@@ -1,4 +0,0 @@
-eget2 github.com/ivan-hc/Steam-appimage --asset=".AppImage" --asset="^zsync" --download-only && {
-    COMPRESSION_OPTS="-l7 -C pcmaudio/waveform::flac:level=8:exhaustive --categorize=pcmaudio --metadata-compression null -S 25 -B 16 --order nilsimsa -W 12 -w 4" BS2AppBundle ./Steam*.AppImage && \
-    mv Steam*.AppBundle "com.valvesoftware.Steam-$(date +%d_%m_%Y).dwfs.AppBundle"
-}
