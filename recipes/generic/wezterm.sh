a795e0f4918e6acac33594e98d6735072e90cd68
xplshn <114888778+xplshn@users.noreply.github.com>
2025-04-20 20:05:42 +0000

    Delete recipes/generic/wezterm.sh

--- recipes/generic/wezterm.sh
+++
@@ -1,9 +0,0 @@
-pelfCreator -m "xplshn" -n "org.wezfurlong.wezterm" -p "wezterm" -e "wezterm-gui" -x "usr/bin/wezterm-gui" -z && {
-    APPDIR="$(echo org.wezfurlong.wezterm*.AppDir)"
-    if ! wget -qO "$APPDIR/.DirIcon" "https://i.ibb.co/0MfXRdq/icon.png"; then
-    	echo "Failed to 128x128 icon"
-    	exit 1
-    fi
-    ID="$(basename "$APPDIR")"
-    pelf --add-appdir "$APPDIR" --appbundle-id "$ID" --output-to "$ID.dwfs.AppBundle"
-}
