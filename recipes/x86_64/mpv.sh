5694662836def418b4bd10a72bb763639c130f86
xplshn <114888778+xplshn@users.noreply.github.com>
2025-04-23 05:00:35 +0000

    Delete recipes/x86_64 directory

--- recipes/x86_64/mpv.sh
+++
@@ -1,4 +0,0 @@
-eget2 github.com/pkgforge-dev/mpv-AppImage --asset=".AppImage" --asset="^zsync" --download-only && {
-    BS2AppBundle ./mpv-*.AppImage
-    mv ./mpv-*dwfs.AppBundle "io.mpv.Mpv-$(date +%d_%m_%Y).$APPBUNDLE_FS.AppBundle"
-}
