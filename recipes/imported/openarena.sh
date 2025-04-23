74cf8d9ffa2210bbce3aaf9dfd869c594a767602
xplshn <114888778+xplshn@users.noreply.github.com>
2025-04-23 21:19:41 +0000

    Create openarena.sh

---
+++ recipes/imported/openarena.sh
@@ -0,0 +1,3 @@
+curl -LO https://github.com/pkgforge-dev/openArena/releases/latest/download/openarena-quake3e.dwfs.AppBundle
+curl -LO https://github.com/pkgforge-dev/openArena/releases/latest/download/quake3e.dwfs.AppBundle
+chmod +x *.dwfs.AppBundle
