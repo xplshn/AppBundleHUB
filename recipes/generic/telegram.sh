0ea290cb4aa5021984bebd39b8d54cdcd2b73bc4
xplshn <114888778+xplshn@users.noreply.github.com>
2025-04-23 17:44:37 +0000

    Update telegram.sh

--- recipes/generic/telegram.sh
+++ recipes/generic/telegram.sh
@@ -1,1 +1,1 @@
-pelfCreator  -m "xplshn" -n "org.telegram.desktop" -p "telegram-desktop" -e "org.telegram.desktop.desktop" -s #-k "lib usr/lib usr/bin/telegram-desktop" -r "lib/firmware"
+pelfCreator  -m "xplshn" -n "telegram" -i "org.telegram.desktop" -p "telegram-desktop" -e "org.telegram.desktop.desktop" -s #-k "lib usr/lib usr/bin/telegram-desktop" -r "lib/firmware"
