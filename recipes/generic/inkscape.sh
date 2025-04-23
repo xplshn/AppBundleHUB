e2ac254a7b18a9e12fb15821c2c0389cae923555
xplshn <114888778+xplshn@users.noreply.github.com>
2025-04-23 17:47:07 +0000

    Update inkscape.sh

--- recipes/generic/inkscape.sh
+++ recipes/generic/inkscape.sh
@@ -1,8 +1,7 @@
-pelfCreator -m "xplshn" -n "org.inkscape.Inkscape" -p "inkscape adwaita-icon-theme" -e "org.inkscape.Inkscape.desktop" -s -z && {
+pelfCreator -m "xplshn" -n "inkscape" -i "org.inkscape.Inkscape" -p "inkscape adwaita-icon-theme" -e "org.inkscape.Inkscape.desktop" -s -z && {
   APPDIR="$(echo org.inkscape.Inkscape*.AppDir)"
   "$APPDIR/AppRun" --Xbwrap --uid "0" --gid "0" -- gtk-update-icon-cache /usr/share/icons/Adwaita
   "$APPDIR/AppRun" --Xbwrap --uid "0" --gid "0" -- gtk-update-icon-cache /usr/share/icons/hicolor
   wget -qO "$APPDIR/AppRun" "https://raw.githubusercontent.com/xplshn/pelf/refs/heads/pelf-ng/assets/AppRun.sharun.ovfsProto"
-  ID="$(basename "$APPDIR")"
-  pelf --add-appdir "$APPDIR" --appbundle-id "$ID" --output-to "$ID.dwfs.AppBundle"
+  pelf --add-appdir "$APPDIR" --appbundle-id "$ID" --output-to "inkscape.dwfs.AppBundle"
 }
