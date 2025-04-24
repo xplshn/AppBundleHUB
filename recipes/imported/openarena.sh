b207598dc0103829cdf5b26afc37c86fa48c1665
xplshn <114888778+xplshn@users.noreply.github.com>
2025-04-24 05:06:47 +0000

    Update openarena.sh

--- recipes/imported/openarena.sh
+++ recipes/imported/openarena.sh
@@ -2,5 +2,5 @@
 curl -LO https://github.com/pkgforge-dev/openArena/releases/latest/download/openarena-quake3e.dwfs.AppBundle
 curl -LO https://github.com/pkgforge-dev/openArena/releases/latest/download/quake3e.dwfs.AppBundle
 chmod +x *.dwfs.AppBundle
-./quake3e.dwfs.AppBundle --pbundle_appstream | base64 -d
-./openarena-quake3e.dwfs.AppBundle --pbundle_appstream | base64 -d
+time sh -c "./quake3e.dwfs.AppBundle --pbundle_appstream | base64 -d"
+time sh -c "./openarena-quake3e.dwfs.AppBundle --pbundle_appstream | base64 -d"
