220d33a15271139cc02fce10e81f2bc3c58bd717
xplshn <114888778+xplshn@users.noreply.github.com>
2025-04-24 17:48:12 +0000

    Update falkon.sh

--- recipes/generic/falkon.sh
+++ recipes/generic/falkon.sh
@@ -1,1 +1,1 @@
-pelfCreator -m "xplshn" -n "falkon" -i "org.kde.falkon" -p "falkon" -e "org.kde.falkon.desktop"
+pelfCreator -m "xplshn" -n "falkon" -i "org.kde.falkon" -p "falkon" -e "org.kde.falkon.desktop" -s
