0d5a703fee5ffcce5674e1aa1ace9f317f67e9b8
xplshn <114888778+xplshn@users.noreply.github.com>
2025-04-24 04:40:31 +0000

    Delete recipes/generic/yt-x.sh

--- recipes/generic/yt-x.sh
+++
@@ -1,7 +0,0 @@
-pelfCreator  -m "xplshn" -n "yt-x" -p "bash" -e "yt-x" -z && {
-    APPDIR="$(echo yt-x*.AppDir)"
-    ID="$(basename "$APPDIR")"
-    DBIN_INSTALL_DIR="./$APPDIR/proto/usr/local/bin" sh -c "dbin list | grep 'jq\|curl\|yt-dlp\|ffmpeg\|fzf\|gum\|chafa\|icat\|imgcat' | xargs dbin add"
-    wget -qO "./$APPDIR/proto/usr/local/bin/yt-x" "https://github.com/Benexl/yt-x/raw/refs/heads/master/yt-x"
-    pelf --add-appdir "$APPDIR" --appbundle-id "$ID" --output-to "$ID.dwfs.AppBundle"
-}
