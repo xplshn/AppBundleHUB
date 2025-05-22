af060149e6834ef6fda1afd1c67398287d0fd707
xplshn <114888778+xplshn@users.noreply.github.com>
2025-05-22 18:23:24 -0300

    Update onlyoffice.sh

--- recipes/archLinux/onlyoffice.sh
+++ recipes/archLinux/onlyoffice.sh
@@ -1,1 +1,1 @@
-pelfCreator -m "xplshn" -n "onlyoffice" -i "org.onlyoffice.desktopeditors" -p "onlyoffice-bin" -e "onlyoffice-desktopeditors.desktop" --local "$BASE_ARCHLINUX"
+pelfCreator -m "xplshn" -n "onlyoffice" -i "org.onlyoffice.desktopeditors" -p "onlyoffice-bin" -e "onlyoffice-desktopeditors.desktop" -s --local "$BASE_ARCHLINUX"
