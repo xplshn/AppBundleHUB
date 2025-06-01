6edd889bac58aa9173ba4dac7e719928eafb6d66
xplshn <114888778+xplshn@users.noreply.github.com>
2025-06-01 03:23:46 -0300

    Update steam.sh

--- recipes/imported/steam.sh
+++ recipes/imported/steam.sh
@@ -1,3 +1,21 @@
-URL="$(curl -Ls https://api.github.com/repos/ivan-hc/Steam-appimage/releases/latest | sed 's/[()",{} ]/\n/g' | grep -o "https.*AppBundle$" | tr '\n' ' ')"
+#!/bin/sh -u
+
+NAME="steam"
+MAINTAINER="xplshn"
+#DATE="$(date +%Y%m%d)"
+APPBUNDLE_ID="${NAME}-${DATE}-${MAINTAINER}"
+ARCH="$(uname -m)"
+
+URL="$(curl -Ls https://api.github.com/repos/ivan-hc/Steam-appimage/releases/latest | sed 's/[()",{} ]/\n/g' | grep -o "https.*Steam-.*-anylinux-${ARCH}\.AppImage$" | tr '\n' ' ')"
+FNAME="$(basename "$URL")"
+
+# Download and extract
 curl -LOs $URL
-chmod +x *.AppBundle
+chmod +x $FNAME
+./$FNAME --appimage-extract && {
+  pelf --add-appdir "${APPBUNDLE_ID}.AppDir" \
+       --output-to "${NAME}.dwfs.AppBundle"  \
+       --appbundle-id "${APPBUNDLE_ID}"
+}
+
+rm "$FNAME"
