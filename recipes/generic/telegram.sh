0d497a5ac0abd2d32aa261add082f754e3796d96
xplshn <114888778+xplshn@users.noreply.github.com>
2025-06-13 00:21:06 -0300

    Delete recipes/generic/telegram.sh

--- recipes/generic/telegram.sh
+++
@@ -1,1 +0,0 @@
-pelfCreator  -m "xplshn" -n "telegram" -i "org.telegram.desktop" -p "telegram-desktop" -e "org.telegram.desktop.desktop" -s #-k "lib usr/lib usr/bin/telegram-desktop" -r "lib/firmware"
