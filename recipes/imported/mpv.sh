e9c98871ef3c23e7d95fc9d4c04aaeb2f74bb03a
xplshn <114888778+xplshn@users.noreply.github.com>
2025-06-19 20:00:08 -0300

    Update mpv.sh

--- recipes/imported/mpv.sh
+++ recipes/imported/mpv.sh
@@ -6,7 +6,7 @@ DATE="$(date +%d_%m_%Y)"
 APPBUNDLE_ID="${NAME}-${DATE}-${MAINTAINER}"
 ARCH="$(uname -m)"
 
-URL="$(curl -Ls https://api.github.com/repos/pkgforge-dev/mpv-AppImage/releases/latest | sed 's/[()",{} ]/\n/g' | grep -o "https.*mpv.*-anylinux-${ARCH}\.dwfs.AppBundle$" | tr '\n' ' ')" #'
+URL="$(curl -Ls https://api.github.com/repos/pkgforge-dev/mpv-AppImage/releases/latest | sed 's/[()",{} ]/\n/g' | grep -o "https.*mpv.*-anylinux-${ARCH}\\.dwfs\\.AppBundle$" | head -n1)"
 FNAME="$APPBUNDLE_ID.dwfs.AppBundle" #FNAME="$(basename "$URL")"
 
 # Download and extract
