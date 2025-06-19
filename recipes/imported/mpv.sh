82d3feae75cbc207c45491b8ba2cd808a213cb1b
xplshn <114888778+xplshn@users.noreply.github.com>
2025-06-19 17:20:13 -0300

    Create mpv.sh

---
+++ recipes/imported/mpv.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -u
+
+NAME="mpv"
+MAINTAINER="xplshn"
+DATE="$(date +%d_%m_%Y)"
+APPBUNDLE_ID="${NAME}-${DATE}-${MAINTAINER}"
+ARCH="$(uname -m)"
+
+URL="$(curl -Ls https://api.github.com/repos/pkgforge-dev/mpv-AppImage/releases/latest | sed 's/[()",{} ]/\n/g' | grep -o "https.*mpv.*-anylinux-${ARCH}\.dwfs.AppBundle$" | tr '\n' ' ')" #'
+FNAME="$APPBUNDLE_ID.dwfs.AppBundle" #FNAME="$(basename "$URL")"
+
+# Download #and extract
+curl -Ls "$URL" -o "$FNAME"
+chmod +x "$FNAME"
+
+#/$FNAME --appimage-extract && {
+#  pelf --add-appdir "./squashfs-root" \
+#       --output-to "${APPBUNDLE_ID}.dwfs.AppBundle"  \
+#       --appbundle-id "${APPBUNDLE_ID}"
+#}
+#rm -rf "$FNAME" "./squashfs-root" "./AppDir"
