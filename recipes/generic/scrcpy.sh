5b14766f243b91565e40afe230460f384ae1aa36
xplshn <114888778+xplshn@users.noreply.github.com>
2025-06-24 14:28:49 -0300

    Update scrcpy.sh

--- recipes/generic/scrcpy.sh
+++ recipes/generic/scrcpy.sh
@@ -1,6 +1,6 @@
 pelfCreator -m "xplshn" -n "scrcpy" -p "scrcpy pipewire-pulse android-tools" -x "usr/bin/scrcpy usr/bin/adb usr/bin/fastboot" -k "usr/share/scrcpy usr/share/android-tools" -e "scrcpy.desktop" -z && {
     APPDIR="$(echo scrcpy*.AppDir)"
     echo "scrcpy" >"$APPDIR/entrypoint"
-    ID="$(basename "$APPDIR")"
+    ID="$(basename "$APPDIR" .AppDir)"
     pelf --add-appdir "$APPDIR" --appbundle-id "$ID" --output-to "$ID.dwfs.AppBundle"
 }
