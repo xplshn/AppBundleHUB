ce5e9bb6c3b5939d8a7b17c66b8f6acbf8ea4c6d
xplshn <anto@xplshn.com.ar>
2025-06-30 10:24:48 -0300

    imported/obs.sh: Get VERSION from URL
    Signed-off-by: xplshn <anto@xplshn.com.ar>

--- recipes/imported/obs.sh
+++ recipes/imported/obs.sh
@@ -11,8 +11,11 @@ URL="$(curl -Ls https://api.github.com/repos/${OWNER}/"$(basename "$REPO")"/rele
   | grep -i "^http.*${NAME}.*${ARCH}.*\\.dwfs\\.AppBundle$" \
   | head -n1)"
 
-# reference: mpv-0.38.0-anylinux-x86_64.dwfs.AppBundle
-VERSION="$(basename "$URL" | awk -F- '{print $2}' | tr '[:upper:]' '[:lower:]')"
+
+## reference: mpv-0.38.0-anylinux-x86_64.dwfs.AppBundle
+##VERSION="$(basename "$URL" | awk -F- '{print $2}' | tr '[:upper:]' '[:lower:]')"
+# Get version from URL
+VERSION=$(echo "$URL" | sed 's:.*/::' | sed 's/[^0-9]*\([0-9][0-9.]*\).*/\1/')
 
 NAME="$(echo "$NAME" | tr '[:upper:]' '[:lower:]')"
 REPO="$(echo "$REPO" | tr '/' '.' | tr '[:upper:]' '[:lower:]')"
