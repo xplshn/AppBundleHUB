5c7c459a5271ab180c6293e3231819f2f2796b64
xplshn <114888778+xplshn@users.noreply.github.com>
2025-06-30 01:58:28 -0300

    Update mousepad.sh

--- recipes/generic/mousepad.sh
+++ recipes/generic/mousepad.sh
@@ -1,1 +1,1 @@
-pelfCreator -m "xplshn" -n "mousepad" -i "org.xfce.mousepad" -p "mousepad" -x "usr/bin/mousepad" -e "org.xfce.mousepad.desktop"
+pelfCreator -m "xplshn" -n "mousepad" -a "org.xfce.mousepad" -p "mousepad" -x "usr/bin/mousepad" -e "org.xfce.mousepad.desktop"
